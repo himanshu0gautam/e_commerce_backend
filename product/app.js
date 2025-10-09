4eee43ec1becdd81462135ae346f1af05667e235
Himanshu <himanshu0000gautam@gmail.com>
2025-10-09 17:48:09 +0530

    not-imp

--- product/app.js
+++ product/app.js
@@ -1,9 +1,11 @@
 import express from 'express';  
 import sellerProductRoute from "./src/routes/seller.product.js"
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
